4d2ac18c674597288eed09916822c5ebd6f77f25
Konstantin Belousov <kombuchamp@gmail.com>
2020-09-06 00:25:07 +0300

    Updates

---
+++ precache-manifest.af0e985afcd3dc68cd1528dcc1338ecb.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95d693d92480415547873d6a1f5e1058",
+    "url": "/dice-roller/index.html"
+  },
+  {
+    "revision": "5d30826adb5b298f3e1a",
+    "url": "/dice-roller/static/css/main.1fadd162.chunk.css"
+  },
+  {
+    "revision": "36ee428a2c9a349f8c06",
+    "url": "/dice-roller/static/js/2.a07b8a1f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dice-roller/static/js/2.a07b8a1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d30826adb5b298f3e1a",
+    "url": "/dice-roller/static/js/main.b19540e1.chunk.js"
+  },
+  {
+    "revision": "1fef78c38f42099e7f04",
+    "url": "/dice-roller/static/js/runtime-main.f9aa7fc1.js"
+  },
+  {
+    "revision": "bd35fed253928e880f9ee7e04ff8c223",
+    "url": "/dice-roller/static/media/d10.bd35fed2.svg"
+  },
+  {
+    "revision": "4b25e80a541f1fc83b82943ca56e0877",
+    "url": "/dice-roller/static/media/d12.4b25e80a.svg"
+  },
+  {
+    "revision": "1e2a0cb6e369f916aa2f5d55dac0008a",
+    "url": "/dice-roller/static/media/d20.1e2a0cb6.svg"
+  },
+  {
+    "revision": "90e4af94b83f2a2fd3816c44eb77af4b",
+    "url": "/dice-roller/static/media/d4.90e4af94.svg"
+  },
+  {
+    "revision": "747179168532db63c70713ca71a84240",
+    "url": "/dice-roller/static/media/d6.74717916.svg"
+  },
+  {
+    "revision": "ddfa6e2905fbe129fd8f387dd83f80c6",
+    "url": "/dice-roller/static/media/d8.ddfa6e29.svg"
+  }
+]);
